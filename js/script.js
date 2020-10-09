a5a731a7f20e283f57293332cccb919aef1e1f06
Kamago <kresnaimp11@gmail.com>
2020-10-09 13:08:31 +0700

    jquery what-we-do

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ function animasiIntroOut(){
                                             duration:1000,
                                             complete: function(){
                                                 callMenu();
-                                                $("#menu ul li a[href='what-we-do']").trigger("click");
+                                                $("#menu ul li a[href='what_we_do']").trigger("click");
                                             }
                                         });
                     }
@@ -39,9 +39,20 @@ function callMenu(){
                                 stagger: 150
                             });
 
-    $("#menu ul li a").click(function(){
+    $("#menu ul li a").click(function(event){
+        event.preventDefault();
         $(this).parent("li").addClass("active").siblings().removeClass("active");
+
+        var hrefString =$(this).attr("href");
+        $("#" + hrefString).show();
+        windows[hrefString]();
     });
+}
+
+function what_we_do(){
+    $("#what_we_do img").velocity("transition.flipYin", {duration:1500});
+    $("#what_we_do .title").velocity("transition.slideUpIn", {duration:1500});
+    $("#what_we_do img").velocity("transition.slideDownIn", {duration:1500});
 }
 
 $(document).ready(function(){
